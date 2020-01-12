3c0114287a7d6b5e470605d485b8d24394d4b19f
Abdolkarim Saeedi <parsa592323@gmail.com>
2020-01-12 15:27:01 +0330

    Add files via upload

---
+++ Flask_FacialRecognition_WebService/Face_Site.py
@@ -0,0 +1,21 @@
+import cv2
+
+
+def Predict(imagePath):
+
+    face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+    eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
+    img = cv2.imread(imagePath)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+
+    for (x, y, w, h) in faces:
+        cv2.rectangle(img, (x, y), (x + w, y + h), (0, 0, 255), 2)
+        roi_gray = gray[y:y + h, x:x + w]
+        roi_color = img[y:y + h, x:x + w]
+        eyes = eye_cascade.detectMultiScale(roi_gray)
+        for (ex, ey, ew, eh) in eyes:
+            cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (255, 0, 0), 2)
+    return img
+
